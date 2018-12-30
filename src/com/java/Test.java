feed7188aea465db5217c659ee37a2863d5c851f
Pawan <pawan.km.m@gmail.com>
2018-12-30 15:29:11 +0530

    fix completed

--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello Java2");
-		System.out.println("Hello Java3");
+		System.out.println("Hello Java32");
 		
 
 	}
